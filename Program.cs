483fce826e6533b3b2ff14f3659fadb4926eb16c
Enjae Antonio <Enjaeantonio@outlook.com>
2023-01-16 09:42:06 -0600

    Used another method for merging two arrays, that I found online

--- Program.cs
+++ Program.cs
@@ -1,9 +1,12 @@
-﻿//We have a list of integers where elements appear either once or twice.
+﻿// Had to delete and remake my git repo for this specific lab as I accidentally made my whole solution a git repo.
+// Sorry if theres no pull requests
+
+//We have a list of integers where elements appear either once or twice.
 //Find the elements that appeared twice in O(n) time.
 //example: { 1, 2, 3, 4, 7, 9, 2, 4}
 //returns
 //{ 2, 4}
-
+Console.WriteLine("Finding elements that appear twice in a list");
 List<int> intList = new List<int> { 1, 2, 3, 4, 7, 9, 2, 4 };
 List<int> duplicateNumbers = new List<int>();
 List<int> nonDuplicates = new List<int>();
@@ -33,13 +36,22 @@ Console.WriteLine($"Non-duplicate numbers are: {nonDuplicateString}");
 //example: { { 1, 2, 3, 4, 5}, { 2, 5, 7, 9, 13} }
 //returns
 //{ 1, 2, 2, 3, 4, 5, 5, 7, 9, 13}
-
 Console.WriteLine();
+Console.WriteLine("Merging two arrays into a third array, and keeping the values sorted");
 int[] array1 = new int[] { 1, 2, 3, 4, 5 };
 int[] array2 = new int[] { 2, 5, 7, 9, 13 };
 int[] array3 = new int[array1.Length + array2.Length];
+Array.Copy(array1, array3, array1.Length);
+Array.Copy(array2, 0, array3, array1.Length, array2.Length);
+Array.Sort(array3);
 
-for(int i = 0; i < array1.Length; i++)
+Console.Write("Method I found online, using the copy method: ");
+foreach(int num in array3)
+{
+    Console.Write(num);
+}
+
+for (int i = 0; i < array1.Length; i++)
 {
     array3[i] = array1[i];
 }
@@ -50,11 +62,12 @@ for(int j = 0; j < array2.Length; j++)
 
 Array.Sort(array3);
 string sortedArray = string.Join(", ", array3);
-Console.WriteLine($"Merged sorted array: {sortedArray}");
+Console.WriteLine();
+Console.WriteLine($"Merged sorted array (my method): {sortedArray}");
 
 //Given an integer, reverse the digits of that integer, e. g. input is 3415,
 //output is 5143. What is the time complexity of your solution?
-
+Console.WriteLine();
 Console.WriteLine("Enter an integer to be reversed");
 int userInt = Int32.Parse(Console.ReadLine());
 char[] chars = userInt.ToString().ToCharArray();
